ae855b2b6ab7d247905a3b572f542a43c2bbebb9
Low_be <69305921+e-NCa@users.noreply.github.com>
2020-08-24 19:35:01 +0900

    Add files via upload

--- test.go
+++ test.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"net/http"
 )
 
+type Page struct {
+	Title string
+	Count int
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	page := Page{"hello", 1}
+	tmpl, err := template.ParseFiles("test.html")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(w, page)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.ListenAndServe(":8080", nil)
 }
